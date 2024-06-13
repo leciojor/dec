15fa9c28b2a2bc69239572288d1b70d92ee8ffb5
leciojor <leciojor@oraciosdevice>
2024-06-12 20:56:28 -0600

    init again

--- index.py
+++ index.py
@@ -1,13 +1,35 @@
 import streamlit as st
-
-
+from streamlit import session_state as state
+import logging
+from langchain_community.chat_models import ChatOllama
 
 def main():
+    logging.basicConfig(filename='log.txt', level=logging.INFO)
+
     st.title("D.E.C.")
+    llm = ChatOllama(model="wizardml2")
 
-    user_input = st.text_input("Initial Prompt: ")
+    if 'user_input' not in st.session_state:
+        state['user_input'] = ''
+    if 'initiate' not in st.session_state:
+        state['initiate'] = False
+    if 'response' not in st.session_state:
+        state['response'] = ''
 
+        
+    state['user_input'] = st.text_input("PERGUNTE O QUE QUISER: ")
+    state['initiate'] = st.button('Iniciar')
 
+    if state['initiate']:
+        logging.info(f'User input: {state["user_input"]} initiated')
+        try:
+            chat_model_response = llm.invoke(state['user_input'])
+            st.success(chat_model_response)
+
+        except Exception as e:
+            st.error(e)
+        
+        
 
 
 
