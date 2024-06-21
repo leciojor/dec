740a4996095b5ee4983538bfc76672d5e8c36b43
Lecio Ribeiro <139591377+leciojor@users.noreply.github.com>
2024-06-20 19:49:22 -0600

    crewai logic

--- index.py
+++ index.py
@@ -2,11 +2,40 @@ import streamlit as st
 from streamlit import session_state as state
 import logging
 from langchain_community.chat_models import ChatOllama
+from crewai import Agent, Task
+from textwrap import dedent
+
+
+def defineTeamAgents(self, role="", goal="", backstory="", tools=[], temperature = 0.8):
+        logging.info(f"Defining team agent with role: {role}")
+        return Agent(
+            role=role,
+            goal=dedent(goal),
+            backstory=dedent(backstory),
+            tools=tools,
+            allow_delegation=False,
+            llm=ChatOllama(model="wizardml2", base_url="http://localhost:11434/", temperature=temperature),
+            verbose=True,
+        )
+
+
+
+def taskDefinition(self, agent, goal = "", expected_output ="", context = ""):
+    return Task(description=dedent(f"""\
+        Do the following: {goal}.
+
+        """),
+        expected_output=expected_output,
+        agent=agent,
+        context=context
+    )
+
 
 def main():
     logging.basicConfig(filename='log.txt', level=logging.INFO)
 
-    st.title("D.E.C.")
+    st.title("Projeto DECI")
+
     llm = ChatOllama(model="wizardml2", base_url="http://localhost:11434/")
 
     if 'user_input' not in st.session_state:
@@ -16,8 +45,7 @@ def main():
     if 'response' not in st.session_state:
         state['response'] = ''
 
-        
-    state['user_input'] = st.text_input("PERGUNTE O QUE QUISER: ")
+    state['user_input'] = st.text_input("REVELE SEUS DESEJOS: ")
     state['initiate'] = st.button('Iniciar')
 
     if state['initiate']:
